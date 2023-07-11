d864116027c20daa1a5af6e59319682f2171cea7
Jordy <jordycorporan95@gmail.com>
2023-07-11 15:48:07 -0400

    Phase 5

--- GreenHouse/src/components/Thermometer/Thermometer.js
+++ GreenHouse/src/components/Thermometer/Thermometer.js
@@ -1,14 +1,16 @@
 import ReactSlider from "react-slider";
 import './Thermometer.css';
+import { useClimate } from "../../context/ClimateContext";
 
 function Thermometer() {
+  const {climate,setClimate} = useClimate();
   return (
     <section>
       <h2>Thermometer</h2>
-      <div className="actual-temp">Actual Temperature: {"x"}°F</div>
+      <div className="actual-temp">Actual Temperature: {climate}°F</div>
       <ReactSlider
-        value={40}
-        onAfterChange={(val) => {}}
+        value={climate}
+        onAfterChange={(val) => setClimate(val)}
         className="thermometer-slider"
         thumbClassName="thermometer-thumb"
         trackClassName="thermometer-track"
--- GreenHouse/src/context/ClimateContext.js
+++ GreenHouse/src/context/ClimateContext.js
@@ -1,2 +1,24 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+
+import { createContext, useContext, useState } from "react";
+
+export const ClimateContext = createContext();  //? creating context
+
+
+
+
+export const useClimate = () => useContext(ClimateContext);  //? using context
+
+export default function ClimateProvider( props) {  // can use props without curlies or {children} with curly
+  const [climate, setClimate] = useState(50);
+  const [humidity, setHumidity] = useState(40);
+  return (
+
+    <ClimateContext.Provider value={{ climate, setClimate }}>
+
+      {props.children}
+    </ClimateContext.Provider>
+
+  );
+}
